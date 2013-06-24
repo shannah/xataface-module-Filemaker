de95aa856bad8d92ac872288f644460b8427f97b
shannah <shannah@a5bf82c2-4c1e-0410-907c-956ee4d02f49>
2013-06-24 22:25:48 +0000

    Fixed Filemaker module so that icon works if module is installed in the application modules directory.

--- Filemaker.php
+++ Filemaker.php
@@ -11,4 +11,32 @@
  * @copyright 2008 Steve Hannah.  All rights reserved.
  *
  */
-class modules_Filemaker{}
+class modules_Filemaker{
+    
+    /**
+    * @brief The base URL to the datepicker module.  This will be correct whether it is in the 
+    * application modules directory or the xataface modules directory.
+    *
+    * @see getBaseURL()
+    */
+    private $baseURL = null;
+    
+    public function __construct(){
+        Dataface_Application::getInstance()->_conf['XF_MODULES_FILEMAKER_BASEURL'] = $this->getBaseURL();
+    }
+    
+    /**
+    * @brief Returns the base URL to this module's directory.  Useful for including
+    * Javascripts and CSS.
+    *
+    */
+   public function getBaseURL(){
+           if ( !isset($this->baseURL) ){
+                   $this->baseURL = Dataface_ModuleTool::getInstance()->getModuleURL(__FILE__);
+           }
+           return $this->baseURL;
+   }
+    
+    
+    
+}
